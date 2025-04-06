5b646e5d79477d6b706fe56a3ea6229110d6fb01
Webermanoel <webermanoel22@gmail.com>
2025-04-06 13:00:17 -0400

    Código atualizado com funcionalidade de adicionar preços

--- src/listafeira.java
+++ src/listafeira.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 
 public class listafeira {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         LinkedList<String> frutas = new LinkedList<>();
+
         System.out.println("Quais frutas deseja adicionar a lista?\n" );
+
         String input = scanner.nextLine();
+
         frutas.add(input);
+
         System.out.println("\nAs frutas adicionadas a lista foram: " + frutas);
 
+        LinkedList<Double> precos = new LinkedList<Double>();
+
+        System.out.println("\nDeseja adicionar preços as frutas?");
+
+        double preco = scanner.nextDouble();
+
+        precos.add(preco);
+
+        System.out.println("\n Os preços adicionados foram: " + precos);
+
+        System.out.println("\nA lista atualizada das frutas com os preços são: \n" + frutas + "R$" + precos);
     }
 }
